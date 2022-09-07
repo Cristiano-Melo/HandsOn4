29f8a9b98a04bef0077459fef7d9d2c690d0ff83
LeoMPG <105465283+LeoMPG@users.noreply.github.com>
2022-09-06 21:28:01 -0300

    Update authMiddleware.ts

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -17,8 +17,7 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
 
     const { id }  = jwt.verify(token, process.env.JWT_PASS ?? "") as JwtPayLoad;
 
-    const user = await userRepository.findOneOrFail({where:{iduser: id}
-    });
+    const user = await userRepository.findOneOrFail({where:{iduser: id}});
 
 
     if(!user) {
