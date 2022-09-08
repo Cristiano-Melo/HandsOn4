a72331dfdb3359d3de59d519178ffc41f509d871
Cristiano Melo <cristianodaniel@gmail.com>
2022-09-07 22:48:24 -0300

    bugfix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { AppDataSource } from './data-source';
 import routes from './routes';
 
+const port = process.env.PORT || 3005
 AppDataSource.initialize().then(()=>{
     const app = express();
 
@@ -9,8 +10,8 @@ AppDataSource.initialize().then(()=>{
 
     app.use(routes);
 
-    app.listen(3005, () => {
-        console.log("Server started on por 3030")
+    app.listen(port, () => {
+        console.info("Server started on http://localhost:"+port)
     })
 
 });
